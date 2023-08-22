1dca84d18e7064aedd2c83c10a6297da06f6be8a
JamesCraft <69867626+James251133@users.noreply.github.com>
2023-08-22 18:27:59 -0400

    Add files via upload

---
+++ Mainsite/server.js
@@ -0,0 +1,81 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const fs = require('fs');
+
+const app = express();
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', (req, res) => {
+    fs.readFile(__dirname + '/home.html', (err, data) => {
+        if (err) throw err;
+        res.end(data);
+    });
+});
+app.get('/home', (req, res) => {
+    fs.readFile(__dirname + '/home.html', (err, data) => {
+        if (err) throw err;
+        res.end(data);
+    });
+});
+
+LobbyGet('Main');
+LobbyGet('Programming');
+LobbyGet('Math');
+LobbyGet('Cubing');
+LobbyGet('Gaming');
+
+
+// Lobby Get
+function LobbyGet(Name) {
+    NameWithSlash = '/' + Name;
+    // Main
+    app.get(NameWithSlash, (req, res) => {
+        fs.readFile(__dirname + '/index.html', (err, data) => {
+            if (err) throw err;
+            res.write(data);
+        });
+        fs.readFile(__dirname + '\\' + '.\\Lobbys\\' + Name + '.txt', (err, data) => {
+            if (err) throw err;
+            res.end(data);
+        });
+    });
+
+    // Messaged
+    app.post(NameWithSlash, (req, res) => {
+        var Message = req.body.Message;
+        var Username = req.body.Username; 
+
+        Message = "<br><div class=\"MessageAll\"><img src=\"./DefaultPfp.jpg\" id=\"MessageImg\"/><h3 id=\"MessageName\">" + Username + "</h3><h2 id=\"Message\">" + Message + "</h2></div><br><br><br>";
+
+        if(Username == undefined || Username == null || Username == "")
+            Message = "";
+
+        fs.appendFileSync('.\\Lobbys\\' + Name + '.txt', Message);
+
+        console.log(Name + ': ' + Message);
+
+        fs.readFile(__dirname + '/index.html', (err, data) => {
+            if (err) throw err;
+            res.write(data);
+        });
+        ReadLobbyData();
+    });
+}
+
+function ReadLobbyData() {
+    fs.readFile(__dirname + '\\' + '.\\Lobbys\\' + Name + '.txt', (err, data) => {
+        if (err) throw err;
+        res.end(data);
+    });
+}
+
+// Images
+app.get('/DefaultPfp.jpg', (req, res) => {
+    res.sendFile(__dirname + '/Images/DefaultPfp.jpg');
+});
+ 
+app.listen(8080, () => {
+    console.log('Started');
+});
